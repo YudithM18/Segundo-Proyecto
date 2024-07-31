7a8981dcd3e4c7d86188d9d05140b5d36d1f3021
YudithM18 <pmarin@fwdcostarica.com>
2024-07-31 16:15:44 -0600

    VALIDE LOS CAMPOS VACIOS O DATOS INCORRECTOS Y LA NAVEGACION ADECUADOS ENTRE LAS 3

--- src/js/index.js
+++ src/js/index.js
@@ -7,26 +7,55 @@ const contraseña = document.getElementById("contraseña")
 
 const btonguardar = document.getElementById("btonguardar")
 
+const texto = document.getElementById("texto")
+
 const lista = JSON.parse(localStorage.getItem("lista") || [])
 
 
 
 
 btonguardar.addEventListener("click", function () {
-    
-   let guardarcorreos = correo.value
-   let guardarusuarios = usuario.value
-   let guardarcontraseñas = contraseña.value
 
-   let persona = {
+   if (correo.value == " " && usuario.value == " " && contraseña.value == " ") {
 
-    gmails : guardarcorreos,
-    users : guardarusuarios,
-    pass : guardarcontraseñas,
+          texto.innerHTML= "INGRESAR DATOS SOLICITADOS"
+      
+   }  else{
 
-   }
+      if (correo.value.length > 0  && usuario.value.length > 0 && contraseña.value.length > 0) {
 
-   lista.push(persona)
+         let guardarcorreos = correo.value
+         let guardarusuarios = usuario.value
+         let guardarcontraseñas = contraseña.value
+
+           let persona = {
+
+            gmails : guardarcorreos,
+            users : guardarusuarios,
+            pass : guardarcontraseñas}
+
+            texto.innerHTML= "USTED HA SIDO REGISTRADO CON EXÍTO"
+
+           lista.push(persona)
  
-   localStorage.setItem("lista", JSON.stringify(lista))
+            localStorage.setItem("lista", JSON.stringify(lista))
+
+
+
+            setTimeout(() => {
+               location.href = "iniciosesion.html"
+            }, 5000);
+
+   
+
+   
+            
+
+         
+      }
+   }
+    
+  
+
+   
 })
--- src/js/iniciosesion.js
+++ src/js/iniciosesion.js
@@ -5,26 +5,41 @@ const password = document.getElementById("password")
 
 const inicio = document.getElementById("inicio")
 
+const texto = document.getElementById("texto")
+
 let datos = JSON.parse(localStorage.getItem("lista") || [])
 
 
 
+" "
+
 inicio.addEventListener("click", function () {
     
-    
-
-
-    console.log(datos);
-
-    for (let index = 0; index < datos.length; index++) {
+     for (let index = 0; index < datos.length; index++) {
         
-  
-   
     if ((datos[index].users === userOrGmail.value || datos[index].gmails === userOrGmail.value) && (datos[index].pass === password.value)  ) {
-        
+         
+        texto.innerHTML = "TUS DATOS SE ENCUENTRAN REGRISTRADO... HAS ENTRADO CON EXÍTO"
 
-    window.location.href = "/Segundo-Proyecto/src/administración.html"
-    } 
+        setTimeout(() => {
+            location.href = "administración.html"
+         }, 3500);
+    } else {
+
+        if ((datos[index].users !== userOrGmail.value || datos[index].gmails !== userOrGmail.value) && (datos[index].pass !== password.value)) {
+            
+            texto.innerHTML ="HAS INGRESADO DATOS INCORRECTOS.."
+            
+        } else{
+
+            if ((datos[index].users == " " || datos[index].gmails == " ") || (datos[index].pass == " ")) {
+
+                texto.innerHTML = "NO SE HA ENCONTRADO DATOS EN DONDE SE SOLICITA, POR FAVOR LLENAR CORRECTAMENTE LOS DATOS SOLICITADOS"
+                
+            }
+        }
+
+    }
         
     }
 })
